83c5a3cdaa87575421c614e3a08254e59b6b5cf0
nono2224 <88888931+nono2224@users.noreply.github.com>
2024-11-03 10:21:16 +0900

    update: 指定ステップのワールドモデルが生成されていないときに，処理がなるよに変更

--- src/app/hook/useLog.ts
+++ src/app/hook/useLog.ts
@@ -8,43 +8,45 @@ export default function useLog() {
     const [simulation, setSimulation] = useState(new Simulation());
 
     useEffect(() => {
-        if (step === 0) {
-            const start = LoadLog.load(process.env.NEXT_PUBLIC_DEFAULT_LOG_PATH, "START_OF_LOG");
-            const end = LoadLog.load(process.env.NEXT_PUBLIC_DEFAULT_LOG_PATH, "END_OF_LOG");
+        if (!simulation.getWorldModel(step)) {
+            if (step === 0) {
+                const start = LoadLog.load(process.env.NEXT_PUBLIC_DEFAULT_LOG_PATH, "START_OF_LOG");
+                const end = LoadLog.load(process.env.NEXT_PUBLIC_DEFAULT_LOG_PATH, "END_OF_LOG");
 
-            Promise.all([start, end]) // start と end の Promise を配列で渡す
-                .then(([startJson, endJson]) => {
-                    // 両方の Promise が解決されたら実行
-                    // then メソッドで Promise を処理
-                    // console.log("in start: ", startJson); // startJson は JSON オブジェクト
-                    // console.log("in end: ", endJson); // endJson は JSON オブジェクト
+                Promise.all([start, end]) // start と end の Promise を配列で渡す
+                    .then(([startJson, endJson]) => {
+                        // 両方の Promise が解決されたら実行
+                        // then メソッドで Promise を処理
+                        // console.log("in start: ", startJson); // startJson は JSON オブジェクト
+                        // console.log("in end: ", endJson); // endJson は JSON オブジェクト
 
-                    if (startJson.hasOwnProperty("start")) {
-                        console.log("start プロパティが存在します");
-                    } else {
-                        console.error("start プロパティは存在しません");
+                        if (startJson.hasOwnProperty("start")) {
+                            console.log("start プロパティが存在します");
+                        } else {
+                            console.error("start プロパティは存在しません");
+                            throw new Error("ログのパスが間違っているか，ログファイルではないか，ログファイルが破損しています");
+                        }
+
+                        if (endJson.hasOwnProperty("end")) {
+                            console.log("end プロパティが存在します");
+                        } else {
+                            console.error("end プロパティは存在しません");
+                            throw new Error("ログのパスが間違っているか，ログファイルではないか，ログファイルが破損しています");
+                        }
+
+                        // start と end の Promise が両方とも正常に解決されたら実行
+                        simulation.setLogPath(String(process.env.NEXT_PUBLIC_DEFAULT_LOG_PATH));
+
+                        simulation.fetchData(step);
+                    })
+                    .catch((error) => {
+                        console.error("ログの読み込みエラー:", error);
+                        // エラー処理
                         throw new Error("ログのパスが間違っているか，ログファイルではないか，ログファイルが破損しています");
-                    }
-
-                    if (endJson.hasOwnProperty("end")) {
-                        console.log("end プロパティが存在します");
-                    } else {
-                        console.error("end プロパティは存在しません");
-                        throw new Error("ログのパスが間違っているか，ログファイルではないか，ログファイルが破損しています");
-                    }
-
-                    // start と end の Promise が両方とも正常に解決されたら実行
-                    simulation.setLogPath(String(process.env.NEXT_PUBLIC_DEFAULT_LOG_PATH));
-
-                    simulation.fetchData(step);
-                })
-                .catch((error) => {
-                    console.error("ログの読み込みエラー:", error);
-                    // エラー処理
-                    throw new Error("ログのパスが間違っているか，ログファイルではないか，ログファイルが破損しています");
-                });
-        } else {
-            // simulation.fetchData(step);
+                    });
+            } else {
+                // simulation.fetchData(step);
+            }
         }
     }, [step]);
 
