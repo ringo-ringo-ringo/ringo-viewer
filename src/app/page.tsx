f50e56a3e2c420b134b52ef73a2e17e00dc69348
nono2224 <88888931+nono2224@users.noreply.github.com>
2024-11-05 15:02:28 +0900

    update: ステップ増加をするためのボタンを配置

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,19 @@
 import useLog from "@/app/hook/useLog";
 import Viewer from "@/app/Viewer";
 import { useEffect } from "react";
+import { Button } from "@mui/material";
 
 export default function Home() {
     const [step, setStep, isPause, setIsPause, simulation, setSimulation] = useLog();
 
+    const stepUp = () => {
+        setStep((step: number) => step + 1);
+    };
+
     return (
         <>
             <Viewer simulation={simulation} step={step}></Viewer>
+            <Button onClick={stepUp} variant="outlined"> go to next step : this step is {step}</Button>
         </>
     );
 }
