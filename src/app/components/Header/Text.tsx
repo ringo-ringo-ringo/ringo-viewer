263e6ab7a91eee7d3e064d287651ff30a77f61c1
nono2224 <88888931+nono2224@users.noreply.github.com>
2024-11-15 17:46:25 +0900

    change: text ds

--- src/app/components/Header/Text.tsx
+++ src/app/components/Header/Text.tsx
@@ -3,7 +3,7 @@ import { css } from "@emotion/react";
 
 export default function Text({ children }: any) {
     const text = css`
-        margin: 0 5px;
+        margin: 0 15px;
     `;
     return (
         <>
