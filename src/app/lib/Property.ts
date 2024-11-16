c6a0cd707f5ec0a32b9ba32021c17ea88ded0987
nono2224 <88888931+nono2224@users.noreply.github.com>
2024-11-16 14:24:29 +0900

    update: waitinglistsize

--- src/app/lib/Property.ts
+++ src/app/lib/Property.ts
@@ -84,9 +84,7 @@ export class Property {
                     this.value = propertys.intvalue;
                     // console.log(this.value);
                 } else if (URN_MAP[propertys.urn] === "WAITING_LIST_SIZE") {
-                    // this.value = propertys.intvalue;
-                    // console.log(this.value);
-                    console.log("何これ : " + URN_MAP[propertys.urn]);
+                    this.value = propertys.intvalue;
                 } else if (URN_MAP[propertys.urn] === "REPAIR_COST") {
                     this.value = propertys.intvalue;
                     // console.log(this.value);
