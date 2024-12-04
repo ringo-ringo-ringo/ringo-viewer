01d4892330ba36910503db8c0fe113b81e76f2a6
nono2224 <88888931+nono2224@users.noreply.github.com>
2024-12-04 22:42:52 +0900

    add: error page

---
+++ src/app/path/page.tsx
@@ -0,0 +1,14 @@
+export default function page() {
+    return (
+        <>
+            <b>ERROR</b>
+            <br></br>
+            <br></br>
+            No log file path provided.
+            <br></br>
+            Please specify the path to the log file you wish to view.
+            <br></br>
+            For example: 'http://localhost:3000/path/rescue.log'
+        </>
+    );
+}
