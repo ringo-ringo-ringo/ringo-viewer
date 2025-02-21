9f4b36f9bf40339a310401b162f0490cfe109d64
nono2224 <88888931+nono2224@users.noreply.github.com>
2025-02-21 15:05:47 +0900

    wip: 自動再生の基盤

--- src/hook/useLog.ts
+++ src/hook/useLog.ts
@@ -4,7 +4,7 @@ import { LoadLog } from "@/lib/LoadLog";
 
 export default function useLog(URLLogPath?: string): [number, Dispatch<SetStateAction<number>>, boolean, Dispatch<SetStateAction<boolean>>, Simulation, Dispatch<SetStateAction<Simulation>>, any, Dispatch<SetStateAction<any>>, number, number] {
     const [step, setStep] = useState<number>(0);
-    const [isPause, setIsPause] = useState<boolean>(true);
+    const [isPause, setIsPause] = useState<boolean>(false);
     const [simulation, setSimulation] = useState<Simulation>(new Simulation());
     const [isLoading, setIsLoading] = useState<number>(0);
     const [perceptionId, setPerceptionId] = useState(null);
@@ -60,6 +60,12 @@ export default function useLog(URLLogPath?: string): [number, Dispatch<SetStateA
                 });
         }
     };
+
+    if (isLoading === 0 && !isPause) {
+        setTimeout(function () {
+            setStep(step + 1);
+        }, 1000);
+    }
 
     useEffect(() => {
         if (!simulation.getWorldModel(step)) {
